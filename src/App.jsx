d2d5a3379a9fa1828faa8928f3af1519e1dfd86f
Alicia Urdiales <aliurdialess@gmail.com>
2023-11-22 17:34:41 +0000

    done till IT4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+//import states
+import { useState } from "react";
+//import data
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+//import estilos
 import "./App.css";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h1>IronNutrition</h1>
+      {/* //* ITERATION 2: 
+       <FoodBox
+        food={{
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1,
+        }}
+      /> */}
+      
+      <FoodBox foodsProp={foods} setFoodsProp = {setFoods} />
+      
     </div>
   );
 }
